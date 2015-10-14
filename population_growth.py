a54b4022a2171edf3324a3c39d42824714e53ce2
COMPPHYS-MESPANOL <mespanol@dons.usfca.edu>
2015-10-13 19:07:46 -0700

    getting names added

--- population_growth.py
+++ population_growth.py
@@ -39,4 +39,37 @@ class Dolphins:
                  procreation = False
             else:
                 List_name.Dolphins(List_name, dolphs, dolphs, random_sex)   #if none of these set procreation to false, procreation instantiates new dolphin
-    
+
+
+#Matthew Espanol
+#Matthew Del Rosario
+#October 13 2015
+import urllib
+import re
+from pprint import pprint as p
+
+counter = 1
+guys_names = []
+lady_names = []
+while counter < 5:
+    first_url = 'http://www.prokerala.com/kids/baby-names/boy/page-'+str(counter)+'.html'
+    second_url = 'http://www.prokerala.com/kids/baby-names/girl/page-'+str(counter)+'.html'
+    file1 = urllib.urlopen(first_url)      
+    lines1 = file1.readlines()
+    file1.close()    
+    file2 = urllib.urlopen(second_url)      
+    lines2 = file2.readlines()
+    file2.close()
+
+    for line1 in lines1:
+        m1 = re.search("(nameDetails\">)([A-Z].*[a-z])<", line1)
+        if m1:
+            guys_names.append(m1.group(2))
+            
+    for line2 in lines2:
+        m2 = re.search("(nameDetails\">)([A-Z].*[a-z])<", line2)
+        if m2:
+            lady_names.append(m2.group(2))
+    counter += 1
+p(guys_names)
+p(lady_names)    
